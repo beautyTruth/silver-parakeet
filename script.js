c8af27d05bcedf2c7ad681e38f3c537d6c97440b
Phil <happy.melodist@gmail.com>
2021-10-02 15:45:45 -0500

    complete dynamic elements with JavaScript

--- script.js
+++ script.js
@@ -1,3 +1,51 @@
+// dynamic year script
+
 const year = document.querySelector(".year");
 const dynamicYear = new Date().getFullYear();
 year.textContent = dynamicYear;
+
+// end dynamic year script
+
+// start other script
+
+const navbar = document.querySelector(".navbar");
+const navbarOffsetTop = navbar.offsetTop;
+const sections = document.querySelectorAll("section");
+const navbarLinks = document.querySelectorAll(".navbar-link");
+const progress = document.querySelector(".progress-bars-wrapper");
+const progressBarPercents = [97, 89, 67, 99, 71, 82, 54];
+
+window.addEventListener("scroll", () => {
+  mainFn();
+});
+
+const mainFn = () => {
+  if (window.pageYOffset >= navbarOffsetTop) {
+    navbar.classList.add("sticky");
+  } else {
+    navbar.classList.remove("sticky");
+  }
+
+  sections.forEach((boobie, i) => {
+    if (window.pageYOffset >= boobie.offsetTop - 10) {
+      navbarLinks.forEach((navbarLink) => {
+        navbarLink.classList.remove("change");
+      });
+      navbarLinks[i].classList.add("change");
+    }
+  });
+
+  if (window.pageYOffset + window.innerHeight >= progress.offsetTop) {
+    document.querySelectorAll(".progress-percent").forEach((el, i) => {
+      el.style.width = `${progressBarPercents[i]}%`;
+      el.previousElementSibling.firstElementChild.textContent =
+        progressBarPercents[i];
+    });
+  }
+};
+
+mainFn();
+
+window.addEventListener("resize", () => {
+  window.location.reload();
+});
